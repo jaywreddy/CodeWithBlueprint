d6d1d58f6360da574d1078513f76e2e55f809729
Martin Meng <martinmeng@berkeley.edu>
2014-02-06 21:34:04 -0800

    another commit

--- CommunityManager.py
+++ CommunityManager.py
@@ -2,15 +2,18 @@ class CommunityManager:
 
     communities = {}
 
-    def add_new_community(name):
-	new_community = Community(name)
+    def add_new_community(self, name):
+	new_community = Community(self, name)
 	communities[new_community.get_id()] = new_community
 
-    def get_community_users(communityId):
+    def get_community_users(self, communityId):
 	communities[communityId].get_users()
 
-    def get_community_articles(communityId):
+    def get_community_articles(self, communityId):
 	communities[communityId].get_articles()
+
+    def get_communityIds(self):
+	return communities.keys()
 
 class Community:
 
@@ -19,24 +22,24 @@ class Community:
     def __init__(self, name):
 	self.name = name
 	self.id = id
-	id += 1
+	Community.id += 1
     	self.users = []
 	self.articles = []
 
-    def add_user(userId):
+    def add_user(self, userId):
 	self.users.append(userId)
 
-    def add_article(articleId):
+    def add_article(self, articleId):
 	self.articles.append(articleId)
 
-    def get_id():
+    def get_id(self):
 	return self.id
 
-    def get_users():
+    def get_users(self):
 	return self.users
 
-    def get_articles():
+    def get_articles(self):
 	return self.articles
     
-    def get_name():
+    def get_name(self):
 	return self.name
